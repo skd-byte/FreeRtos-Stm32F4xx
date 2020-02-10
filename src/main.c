92f656efc29aac7636b38b67b91586f0b7f39dce
SK Dubey <shubham.kant.dubey@gmail.com>
2020-02-10 21:14:11 +0530

    [EXAMPLE_06] Combining blocking and non-blocking tasks.

--- src/main.c
+++ src/main.c
@@ -12,14 +12,14 @@
 /*
  * Tasks
  */
-portTASK_FUNCTION_PROTO(vTask, pvParameters);
+portTASK_FUNCTION_PROTO(vContinuousProcessingTask, pvParameters);
+portTASK_FUNCTION_PROTO(vPeriodicTask, pvParameters);
 
-
-static const char *TaskFirstInsatnceMsg  = "Task1 is running\r\n";
-static const char *TaskSecondInsatnceMsg = "Task2 is running\r\n";
+static const char *TaskFirstInsatnceMsg  = "Continuous Task1 is running\n";
+static const char *TaskSecondInsatnceMsg = "Continuous Task2 is running\n";
+const char *pcTextForPeriodicTask        = "Periodic Task is running\n";
 
 #define mainDELAY_LOOP_COUNT   40000
-
 
 /*
  * define GPIO Board LEDs
@@ -38,10 +38,10 @@ int main(int argc, char* argv[])
   GPIO_Init();
   while (1)
   {
+     xTaskCreate(vContinuousProcessingTask, "Task1", 150, (void*)TaskFirstInsatnceMsg,  1, NULL);
+     xTaskCreate(vContinuousProcessingTask, "Task2", 150, (void*)TaskSecondInsatnceMsg, 1, NULL);
 
-
-     xTaskCreate(vTask,     "Task1", 150, (void*)TaskFirstInsatnceMsg, 1, NULL);
-     xTaskCreate(vTask,     "Task2", 150, (void*)TaskSecondInsatnceMsg, 2, NULL);
+     xTaskCreate( vPeriodicTask, "Task 3", 1000, (void*)pcTextForPeriodicTask, 2, NULL );
      /* Start the scheduler. */
      vTaskStartScheduler();
 
@@ -51,48 +51,61 @@ int main(int argc, char* argv[])
 }
 
 
-
-
- void vApplicationMallocFailedHook( void ){
-
+void vApplicationMallocFailedHook( void )
+{
    trace_printf("Entered vApplicationMallocFailedHook\n");
    for(;;);
- }
+}
 
- void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName){
-
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName)
+{
    ( void )pxTask;
    ( void )pcTaskName;
 
    for(;;);
- }
+}
 
- /*
-  * Tasks
-  */
+/*
+ * Tasks
+ */
+void vContinuousProcessingTask( void *pvParameters )
+{
+  char *pcTaskName;
 
- void vTask( void *pvParameters)
- {
-   char *pcTaskName;
-   TickType_t xLastWakeTime;
-   /* The string to print out is passed in via the parameter. Cast this to a
+   /* The string to print out is passed in via the parameter.  Cast this to a
    character pointer. */
    pcTaskName = ( char * ) pvParameters;
-   /* The xLastWakeTime variable needs to be initialized with the current tick
-   count. Note that this is the only time the variable is written to explicitly.
-   After this xLastWakeTime is automatically updated within vTaskDelayUntil(). */
-   xLastWakeTime = xTaskGetTickCount();
+
    /* As per most tasks, this task is implemented in an infinite loop. */
    for( ;; )
    {
-   /* Print out the name of this task. */
+     /* Print out the name of this task.  This task just does this repeatedly
+     without ever blocking or delaying. */
      trace_printf( pcTaskName );
-   /* This task should execute every 250 milliseconds exactly. As per
-   the vTaskDelay() function, time is measured in ticks, and the
-   pdMS_TO_TICKS() macro is used to convert milliseconds into ticks.
-   xLastWakeTime is automatically updated within vTaskDelayUntil(), so is not
-   explicitly updated by the task. */
-   vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( 250 ) );
+   }
+}
+
+uint32_t count = 5;
+
+void vPeriodicTask( void *pvParameters )
+{
+  TickType_t xLastWakeTime;
+  const TickType_t xDelay5ms = pdMS_TO_TICKS( 5UL );
+
+   /* The xLastWakeTime variable needs to be initialized with the current tick
+   count.  Note that this is the only time we access this variable.  From this
+   point on xLastWakeTime is managed automatically by the vTaskDelayUntil()
+   API function. */
+   xLastWakeTime = xTaskGetTickCount();
+
+   /* As per most tasks, this task is implemented in an infinite loop. */
+   for( ;; )
+   {
+     /* Print out the name of this task. */
+     trace_printf( "Periodic Task is running\n");
+
+     /* We want this task to execute exactly every 5 milliseconds. */
+     vTaskDelayUntil( &xLastWakeTime, xDelay5ms );
    }
 }
 
