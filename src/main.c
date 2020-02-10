31ebdd454767832da61e558e15c1c3f456120b3a
SK Dubey <shubham.kant.dubey@gmail.com>
2020-02-10 19:50:29 +0530

    [EXAMPLE_05] Task execute with a fixed frequency.
    1. Used vTaskDelayUntil() for genrating periodic delay with fix frequency.

--- src/main.c
+++ src/main.c
@@ -73,12 +73,27 @@ int main(int argc, char* argv[])
 
  void vTask( void *pvParameters)
  {
-   while(1)
+   char *pcTaskName;
+   TickType_t xLastWakeTime;
+   /* The string to print out is passed in via the parameter. Cast this to a
+   character pointer. */
+   pcTaskName = ( char * ) pvParameters;
+   /* The xLastWakeTime variable needs to be initialized with the current tick
+   count. Note that this is the only time the variable is written to explicitly.
+   After this xLastWakeTime is automatically updated within vTaskDelayUntil(). */
+   xLastWakeTime = xTaskGetTickCount();
+   /* As per most tasks, this task is implemented in an infinite loop. */
+   for( ;; )
    {
-     trace_printf((char*)pvParameters);
-     /* Delay for a period. */
-     vTaskDelay( pdMS_TO_TICKS( 100 ) );
-  }
+   /* Print out the name of this task. */
+     trace_printf( pcTaskName );
+   /* This task should execute every 250 milliseconds exactly. As per
+   the vTaskDelay() function, time is measured in ticks, and the
+   pdMS_TO_TICKS() macro is used to convert milliseconds into ticks.
+   xLastWakeTime is automatically updated within vTaskDelayUntil(), so is not
+   explicitly updated by the task. */
+   vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( 250 ) );
+   }
 }
 
 void GPIO_Init(void)
