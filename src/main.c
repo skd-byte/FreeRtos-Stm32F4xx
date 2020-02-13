b55ffea451de90fbef2cb7441fa0cfa2cd192644
SK Dubey <shubham.kant.dubey@gmail.com>
2020-02-13 21:25:30 +0530

    [EXAMPLE_10] Message Queue.

--- src/main.c
+++ src/main.c
@@ -11,8 +11,8 @@
 /*
  * Tasks
  */
-portTASK_FUNCTION_PROTO(vTask1, pvParameters);
-portTASK_FUNCTION_PROTO(vTask2, pvParameters);
+portTASK_FUNCTION_PROTO(vTaskSender, pvParameters);
+portTASK_FUNCTION_PROTO(vTaskReceiver, pvParameters);
 
 /* Used to hold the handle of Task2. */
 TaskHandle_t xTask2Handle;
@@ -33,54 +33,113 @@ static const char *Task2Msg  = "Task2 is running\n";
 void Toggle_LED(void);
 void GPIO_Init(void);
 
+/* Declare a variable of type QueueHandle_t. This is used to store the handle
+ to the queue that is accessed by all three tasks. */
+ QueueHandle_t xQueue;
 
 int main(int argc, char* argv[])
 {
   UART_CLIInit();
   GPIO_Init();
-  xTaskCreate( vTask1, "Task 1", 1000, NULL, 1, NULL );
-         /* The task handle is the last parameter ^^^^^^^^^^^^^ */
 
-  /* Start the scheduler to start the tasks executing. */
-  vTaskStartScheduler();
+  xQueue = xQueueCreate( 5, sizeof( int32_t ) );
 
-  /* The following line should never be reached because vTaskStartScheduler()
-  will only return if there was not enough FreeRTOS heap memory available to
-  create the Idle and (if configured) Timer tasks.  Heap management, and
-  techniques for trapping heap exhaustion, are described in the book text. */
+  if(xQueue != NULL)
+  {
+    xTaskCreate( vTaskSender, "Sender1", 1000,(void *) 100, 1, NULL );
+    xTaskCreate( vTaskSender, "Sender2", 1000,(void *) 200, 1, NULL );
+
+    xTaskCreate( vTaskReceiver, "Receiver",1000, NULL,2 ,NULL);
+
+    /* Start the scheduler so the created tasks start executing. */
+    vTaskStartScheduler();
+  }
+  else
+  {
+    // Queue could not be creaated
+  }
+
   for( ;; );
   return 0;
 }
-/*-----------------------------------------------------------*/
 
-void vTask1( void *pvParameters )
+
+void vTaskSender( void *pvParameters )
 {
-  const TickType_t xDelay100ms = pdMS_TO_TICKS( 100UL );
+  int32_t lValueToSend;
+  BaseType_t xStatus;
+  /* Two instances of this task are created so the value that is sent to the
+  queue is passed in via the task parameter - this way each instance can use
+  a different value. The queue was created to hold values of type int32_t,
+  so cast the parameter to the required type. */
+  lValueToSend = ( int32_t ) pvParameters;
+  /* As per most tasks, this task is implemented within an infinite loop. */
   for( ;; )
   {
-    /* Print out the name of this task. */
-    trace_printf( "Task 1 is running\r\n" );
-    /* Create task 2 at a higher priority. Again the task parameter is not
-    used so is set to NULL - BUT this time the task handle is required so
-    the address of xTask2Handle is passed as the last parameter. */
-    xTaskCreate( vTask2, "Task 2", 1000, NULL, 2, &xTask2Handle );
-    /* The task handle is the last parameter _____^^^^^^^^^^^^^ */
-    /* Task 2 has/had the higher priority, so for Task 1 to reach here Task 2
-    must have already executed and deleted itself. Delay for 100
-    milliseconds. */
-    vTaskDelay( xDelay100ms );
+    /* Send the value to the queue.
+    The first parameter is the queue to which data is being sent. The
+    queue was created before the scheduler was started, so before this task
+    started to execute.
+    The second parameter is the address of the data to be sent, in this case
+    the address of lValueToSend.
+    The third parameter is the Block time – the time the task should be kept
+    in the Blocked state to wait for space to become available on the queue
+    should the queue already be full. In this case a block time is not
+    specified because the queue should never contain more than one item, and
+    therefore never be full. */
+    xStatus = xQueueSendToBack( xQueue, &lValueToSend, 0 );
+    if( xStatus != pdPASS )
+    {
+      /* The send operation could not complete because the queue was full -
+      this must be an error as the queue should never contain more than
+      one item! */
+      trace_printf( "Could not send to the queue.\r\n" );
+    }
   }
 }
 
-  /*-----------------------------------------------------------*/
 
-void vTask2( void *pvParameters )
+
+void vTaskReceiver( void *pvParameters )
 {
-  /* Task 2 does nothing but delete itself. To do this it could call vTaskDelete()
-  using NULL as the parameter, but instead, and purely for demonstration purposes,
-  it calls vTaskDelete() passing its own task handle. */
-  trace_printf( "Task 2 is running and about to delete itself\r\n" );
-  vTaskDelete( xTask2Handle );
+  /* Declare the variable that will hold the values received from the queue. */
+  int32_t lReceivedValue;
+  BaseType_t xStatus;
+  const TickType_t xTicksToWait = pdMS_TO_TICKS( 100 );
+  /* This task is also defined within an infinite loop. */
+  for( ;; )
+  {
+    /* This call should always find the queue empty because this task will
+    immediately remove any data that is written to the queue. */
+    if( uxQueueMessagesWaiting( xQueue ) != 0 )
+    {
+      trace_printf( "Queue should have been empty!\r\n" );
+    }
+    /* Receive data from the queue.
+    The first parameter is the queue from which data is to be received. The
+    queue is created before the scheduler is started, and therefore before this
+    task runs for the first time.
+    The second parameter is the buffer into which the received data will be
+    placed. In this case the buffer is simply the address of a variable that
+    has the required size to hold the received data.
+    The last parameter is the block time – the maximum amount of time that the
+    task will remain in the Blocked state to wait for data to be available
+    should the queue already be empty. */
+    xStatus = xQueueReceive( xQueue, &lReceivedValue, xTicksToWait );
+    if( xStatus == pdPASS )
+    {
+      /* Data was successfully received from the queue, print out the received
+      value. */
+      trace_printf( "Received = %d\n", lReceivedValue );
+    }
+    else
+    {
+      /* Data was not received from the queue even after waiting for 100ms.
+      This must be an error as the sending tasks are free running and will be
+      continuously writing to the queue. */
+      trace_printf( "Could not receive from the queue.\r\n" );
+    }
+  }
 }
 
 /* Idle hook functions MUST be called vApplicationIdleHook(), take no parameters,
